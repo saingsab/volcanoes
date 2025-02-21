1c34cdd713dd57cdfbaecf2f9f0d44360c3def6d
saing sab <saing.s-external@webtiga.com>
2025-02-21 18:08:07 +0700

    add csv

--- src/volcanoes/core.clj
+++ src/volcanoes/core.clj
@@ -1,6 +1,22 @@
-(ns volcanoes.core)
+(ns volcanoes.core
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def csv-read (csv/read-csv (io/reader "resources/volcanoes.csv")))
+
+(defn transform-header [header]
+  (if (= "Elevation (m)" header)
+    :elevation-metters
+    (-> header
+        (clojure.string/lower-case)
+        (clojure.string/replace #" " "-")
+        keyword)))
+
+(defn transform-header-row [header-line]
+  (map transform-header header-line)) 
+
+(def volcano-records
+  (let [csv-line (rest csv-read)]
+    (map (fn [line]
+           (zipmap (first csv-read) line))
+         csv-line)))
