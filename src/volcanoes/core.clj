b472bde217fb50466b6bffa79e0def943f317bec
saing sab <saing.s-external@webtiga.com>
2025-03-02 23:44:22 +0700

    B02-05

--- src/volcanoes/core.clj
+++ src/volcanoes/core.clj
@@ -2,21 +2,41 @@
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
-(def csv-read (csv/read-csv (io/reader "resources/volcanoes.csv")))
-
 (defn transform-header [header]
   (if (= "Elevation (m)" header)
-    :elevation-metters
+   :elevation-meters
     (-> header
-        (clojure.string/lower-case)
+        clojure.string/lower-case
         (clojure.string/replace #" " "-")
         keyword)))
 
 (defn transform-header-row [header-line]
-  (map transform-header header-line)) 
+  (map transform-header header-line))
+
+(def csv-lines
+  (with-open [csv (io/reader "resources/volcanoes.csv")]
+    (doall (csv/read-csv csv))))
 
 (def volcano-records
-  (let [csv-line (rest csv-read)]
-    (map (fn [line]
-           (zipmap (first csv-read) line))
-         csv-line)))
+  (let [csv-lines (rest csv-lines)
+        header-line (transform-header-row (first csv-lines))
+        volcano-lines (rest csv-lines)]
+    (map (fn [volcano-line]
+           (zipmap header-line volcano-line))
+         volcano-lines)))
+
+(defn parse-numbers [volcano]
+  (-> volcano
+      (update :elevation-meters #(Integer/parseInt %))
+      (update :longitude #(Double/parseDouble %))
+      (update :latitude #(Double/parseDouble %))))
+(def volcanoes-parsed
+  (map parse-numbers volcano-records))
+
+(comment
+  (let [volcano (nth volcanoes-parsed 100)]
+    (clojure.pprint/pprint volcano))
+
+  (let [volcanoes (first (filter #(= "210010" (:volcano-number %)) volcanoes-parsed))]
+    (clojure.pprint/pprint volcanoes)))
+(def types (set (map :primary-volcano-type volcano-records)))
